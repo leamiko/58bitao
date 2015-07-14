acdfe9f2008c3f8b5ee66d144af96490c1e67ce1
selector <selector@abc360.com>
2015-07-14 14:25:01 +0800

    add mobile view

--- index.php
+++ index.php
@@ -3,6 +3,36 @@ if (!is_file('./data/install.lock')) {
     header('Location: ./install.php');
     exit;
 }
+function isMobileUrl()
+{
+	$pathinfo = $_SERVER['PATH_INFO'];
+	if(strpos($pathinfo,'/m/') === false)
+	{
+		return false;
+	}	
+	return true;
+}
+function isMobileDevice()
+{
+	$agent = strtolower($_SERVER['HTTP_USER_AGENT']);
+	$type = '';
+	$keyword = array('iphone','ipad','ipod','android');
+	foreach ($keyword as $k) 
+	{
+		if(strpos($agent, $k) !== false)
+		{
+			return true;
+		}
+	}
+	return false;
+	
+}
+
+if(isMobileDevice() && !isMobileUrl())
+{
+	header('Location: index.php/m/index/index');
+	exit();
+}
 define('FTX_VERSION', '5.0');
 define('FTX_RELEASE', '20131210');
 define('APP_NAME', 'app');
